392a086fd28d564c57a23d2fe5b848a46f5c8046
alexodonn <alexodonn@gmail.com>
2019-09-26 20:47:18 -0300

    :sparkles: init

---
+++ build/CMakeFiles/crummy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/crummy.dir/src/main.c.o"
+  "crummy.pdb"
+  "crummy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/crummy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
