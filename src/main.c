a0068b9d3fb81c7ccf7c4e6b72edf6555ae8dc0f
alexodonn <alexodonn@gmail.com>
2019-10-04 21:01:43 -0300

    add assets and draw cowboy

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
-#include <stdio.h>
+#include "raylib.h"
 
-int main()
+const int screenWidth = 800;
+const int screenHeight = 450;
+
+int main(int argc, char* argv[])
 {
-    printf("heyo\n");
+    InitWindow(screenWidth, screenHeight, "crummy");
+
+    Texture2D cowboi = LoadTexture("assets/cowboy.png");
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawTexture(cowboi, screenWidth/2 - cowboi.width/2, screenHeight/2 - cowboi.height/2, WHITE);
+
+        EndDrawing();
+    }
+    
+    return 0;
 }
